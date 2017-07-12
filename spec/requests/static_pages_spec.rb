00b17f522a0a87eca811c85f83cccb1350995339
Casual3498 <sergeigpopov@gmail.com>
2017-07-12 23:36:07 +0300

    yes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  # describe "GET /static_pages" do
-  #   it "works! (now write some real specs)" do
-  #     get static_pages_index_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
+
+ let(:base_title) { "Lunches" }
+
   describe "Dashboard page" do
   	it "should have the content dashboard" do
   		visit 'static_pages/dashboard'
@@ -16,7 +13,7 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the title 'Dashboard'" do
       visit '/static_pages/dashboard'
-      expect(page).to have_title("Lunches | Dashboard")
+      expect(page).to have_title("#{:base_title} | Dashboard")
     end
   end
 end
