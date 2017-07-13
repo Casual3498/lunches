78ec1c978ba3431a56a76ba81238d2e128c8cc97
Casual3498 <sergeigpopov@gmail.com>
2017-07-13 12:56:29 +0300

    test mobile

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,22 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the title 'Dashboard'" do
       visit '/static_pages/dashboard'
-      expect(page).to have_title("#{:base_title} | Dashboard")
+      expect(page).to have_title("#{base_title} | Dashboard")
     end
   end
+
+  describe "Dashboard Lunches Admin page" do
+  	it "should have the content Dashboard Lunches Admin" do
+  		visit 'static_pages/dashboard_lunches_admin'
+  		expect(page).to have_content('Dashboard Lunches Admin')
+  	end
+  
+
+    it "should have the title 'Dashboard Lunches Admin'" do
+      visit '/static_pages/dashboard_lunches_admin'
+      expect(page).to have_title("#{base_title} | Dashboard Lunches Admin")
+    end
+  end
+
+
 end
