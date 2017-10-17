58e05f4064e18e79a49a22fa1785c602267e00c5
Casual3498 <sergeigpopov@gmail.com>
2017-10-17 12:00:25 +0300

    travis

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,29 +4,29 @@ RSpec.describe "StaticPages", type: :request do
 
  let(:base_title) { "Lunches" }
 
-  describe "Dashboard page" do
-  	it "should have the content dashboard" do
-  		visit 'static_pages/dashboard'
-  		expect(page).to have_content('Dashboard')
+  describe "Privacy Notice page" do
+  	it "should have the content Privacy Policy" do
+  		visit 'privacy_notice'
+  		expect(page).to have_content('Privacy Policy')
   	end
   
 
-    it "should have the title 'Dashboard'" do
-      visit '/static_pages/dashboard'
-      expect(page).to have_title("#{base_title} | Dashboard")
+    it "should have the title 'Privacy Notice'" do
+      visit 'privacy_notice'
+      expect(page).to have_title("#{base_title} | Privacy Notice")
     end
   end
 
-  describe "Dashboard Lunches Admin page" do
-  	it "should have the content Dashboard Lunches Admin" do
-  		visit 'static_pages/dashboard_lunches_admin'
-  		expect(page).to have_content('Dashboard Lunches Admin')
+  describe "Conditions of Use page" do
+  	it "should have the content Terms of Use" do
+  		visit 'conditions_of_use'
+  		expect(page).to have_content('Terms of Use')
   	end
   
 
-    it "should have the title 'Dashboard Lunches Admin'" do
-      visit '/static_pages/dashboard_lunches_admin'
-      expect(page).to have_title("#{base_title} | Dashboard Lunches Admin")
+    it "should have the title 'Conditions of Use'" do
+      visit 'conditions_of_use'
+      expect(page).to have_title("#{base_title} | Conditions of Use")
     end
   end
 
