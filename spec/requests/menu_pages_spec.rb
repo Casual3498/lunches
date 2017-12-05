24dd83430daaa3b5a7f92b88a1a5ac4469ab57d8
Casual3498 <sergeigpopov@gmail.com>
2017-12-05 21:42:46 +0300

    replace deprecated on :wait (for travis)

--- spec/requests/menu_pages_spec.rb
+++ spec/requests/menu_pages_spec.rb
@@ -86,7 +86,7 @@ RSpec.describe "MenuPages", type: :request do
     describe "lunches admin can add items in menu only for today by adding a name and price" do
       before do
         Capybara.current_driver = :selenium  #set driver for work with js
-        Capybara.default_wait_time = 1
+        Capybara.default_max_wait_time = 1
         sign_in lunches_admin
         visit menus_path
       end
@@ -107,7 +107,7 @@ RSpec.describe "MenuPages", type: :request do
                 fill_in 'Name', with: "shchi №#{n}"
                 fill_in 'Cost', with: "#{(rand()*1000).round(2)}"
                 attach_file("menu[picture]", "#{Rails.root}/spec/support/images/#{n}.jpg")
-                click_button "Create #{course_type.name.downcase} menu item"
+                click_button "Create #{course_type.name.downcase} menu item", wait: 1
               end
             end.to change(Menu, :count).by(5) #check that items really added
             #menu for course must have 5 elements, that we just added
@@ -136,7 +136,7 @@ RSpec.describe "MenuPages", type: :request do
             within(:css, "tbody", id: "menu_index#{course_type.id}") do
               click_link_or_button 'Delete', match: :first
             end
-            click_link_or_button 'Yes, Delete This Menu Item'
+            click_link_or_button 'Yes, Delete This Menu Item', wait: 1
           end.to change(Menu, :count).by(-1) #check that item really deleted
           #menu for course must have 4 elements, because first of 5 just was deleted
           within(:css, "tbody", id: "menu_index#{course_type.id}") do
